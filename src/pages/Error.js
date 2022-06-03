027bcd3005899788a4313a20ace31c766bc58ae6
l-stend <stendardo.lu@gmail.com>
2022-06-03 12:01:57 +0200

    first router setting, pages setting, error page

---
+++ src/pages/Error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='not-found' />
+        <h3>Ooops, page not found</h3>
+        <p>me sa che te tocca il bono della sora Lella</p>
+        <Link to='/'>Back Home</Link>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Error;
