b1137f58b07033d10871d91870eed4832aff264c
l-stend <stendardo.lu@gmail.com>
2022-06-03 20:28:49 +0200

    dashboard's shared layout setup

---
+++ src/components/BigSidebar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BigSidebar = () => {
+  return <div>big sidebar</div>;
+};
+
+export default BigSidebar;
---
+++ src/components/SmallSidebar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SmallSidebar = () => {
+  return <div>small sidebar</div>;
+};
+
+export default SmallSidebar;
