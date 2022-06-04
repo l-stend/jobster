2efa9b940e78eab21765efdd36154f3ef7e6a1dc
l-stend <stendardo.lu@gmail.com>
2022-06-04 21:19:15 +0200

    addjob fine for now

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import FormRow from './FormRow';
 import Navbar from './Navbar';
 import BigSidebar from './BigSidebar';
 import SmallSidebar from './SmallSidebar';
+import FormRowSelect from './FormRowSelect';
 
-export { Logo, FormRow, Navbar, SmallSidebar, BigSidebar };
+export { Logo, FormRow, Navbar, SmallSidebar, BigSidebar, FormRowSelect };
