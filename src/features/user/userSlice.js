3f34f93e0e7425b042042483753b44b8c69946a7
l-stend <stendardo.lu@gmail.com>
2022-06-04 17:28:51 +0200

    profile page, patch req, update function

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,6 +6,11 @@ import {
   getUserFromLocalStorage,
   removeUserFromLocalStorage,
 } from '../../utils/localStorage';
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+} from './userThunk';
 
 const initialState = {
   isLoading: false,
@@ -13,27 +18,73 @@ const initialState = {
   user: getUserFromLocalStorage(),
 };
 
+///VERSIONE COMPLETA DELLE FUNZIONI ASYNCTHUNK (PRIMA DEL REFACTORING) ❗❕❗
+///DA USARE COME RIFERIMENTO PER SCRIVERNE DI NUOVE             ❕❗❕
+
+// export const registerUser = createAsyncThunk(
+//   'action/registerUser',
+//   async (user, ThunkAPI) => {
+//     try {
+//       const res = await customFetch.post('/auth/register', user);
+//       return res.data;
+//     } catch (error) {
+//       return ThunkAPI.rejectWithValue(error.response.data.msg);
+//     }
+//   }
+// );
+
+// export const loginUser = createAsyncThunk(
+//   'action/loginUser',
+//   async (user, ThunkAPI) => {
+//     try {
+//       const res = await customFetch.post('/auth/login', user);
+//       return res.data;
+//     } catch (error) {
+//       return ThunkAPI.rejectWithValue(error.response.data.msg);
+//     }
+//   }
+// );
+
+// export const updateUser = createAsyncThunk(
+//   'user/updateUser',
+//   async (user, thunkAPI) => {
+//     try {
+//       const resp = await customFetch.patch('/auth/updateUser', user, {
+//         headers: {
+//           authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+//         },
+//       });
+//       return resp.data;
+//     } catch (error) {
+//       if (error.response.status === 401) {
+//         thunkAPI.dispatch(logoutUser());
+//         return thunkAPI.rejectWithValue('Unauthorized, logging out...');
+//       }
+//       return thunkAPI.rejectWithValue(error.response.data.msg);
+//     }
+//   }
+// );
+
+//// FUNZIONI DI COMUNICAZIONE COL SERVER DOPO IL REFACTORING ❕❗❕
+//// GUARDA QUELLE COMMENTATE ❗❕❗
+
 export const registerUser = createAsyncThunk(
-  'action/registerUser',
-  async (user, ThunkAPI) => {
-    try {
-      const res = await customFetch.post('/auth/register', user);
-      return res.data;
-    } catch (error) {
-      return ThunkAPI.rejectWithValue(error.response.data.msg);
-    }
+  'user/registerUser',
+  async (user, thunkAPI) => {
+    return registerUserThunk('/auth/register', user, thunkAPI);
   }
 );
 
 export const loginUser = createAsyncThunk(
-  'action/loginUser',
-  async (user, ThunkAPI) => {
-    try {
-      const res = await customFetch.post('/auth/login', user);
-      return res.data;
-    } catch (error) {
-      return ThunkAPI.rejectWithValue(error.response.data.msg);
-    }
+  'user/loginUser',
+  async (user, thunkAPI) => {
+    return loginUserThunk('/auth/login', user, thunkAPI);
+  }
+);
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async (user, thunkAPI) => {
+    return updateUserThunk('/auth/updateUser', user, thunkAPI);
   }
 );
 
@@ -48,6 +99,7 @@ const userSlice = createSlice({
       state.user = null;
       state.isSidebarOpen = null;
       removeUserFromLocalStorage();
+      toast.success('logout successful');
     },
   },
   extraReducers: {
@@ -77,6 +129,21 @@ const userSlice = createSlice({
       toast(`Welcome Back ${user.name}`);
     },
     [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+    [updateUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [updateUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+
+      addUserToLocalStorage(user);
+      toast.success('User Updated');
+    },
+    [updateUser.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
